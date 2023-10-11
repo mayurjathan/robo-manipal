57d07463ed3714f72b052e0113e3cff621672666
mayurjathan <mayurjathan999@gmail.com>
2023-10-11 10:50:12 +0530

    Qn1 added

---
+++ Task1/Qn1.cpp
@@ -0,0 +1,35 @@
+// Write a C++ program to sort a character array alphabetically using bubble sort. 
+// (lower case only). Eg: input: “worksheet” Output: “eehkorstw” 
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+        char word[20];
+        cout << "Enter the Input : ";
+        cin >> word;
+        int n=strlen(word);
+        bool swapped;
+        for(int i =0;i<n-1;i++)
+        {
+                swapped=false;
+                for(int j=0;j<n-i-1;j++)
+                {
+                        if(word[j]>word[j+1])
+                        {
+                                char temp=word[j];
+                                word[j]=word[j+1];
+                                word[j+1]=temp;
+                        }
+                }
+                // if no swap occured in this pass the array is already sorted
+                if(!swapped)
+                {
+                        break;
+                }
+        }
+        
+
+        cout << "The Output is : "<< word;
+}
+
---
+++ Task1/Qn2.cpp
@@ -0,0 +1,2 @@
+// Write a C++ program to multiply two matrices and print the product. 
+// If the entered matrices are not compatible then print a message saying so. 
